f2fb77d434bab83ab4de56852ab2ad82d7faeec0
葛富臻 <1269717060@qq.com>
2015-12-17 14:17:53 +0800

    参照game游戏编写自己的游戏项目testprj

---
+++ testprj1/Classes/HelloWorldScene.cpp
@@ -0,0 +1,109 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    
+	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                origin.y + closeItem->getContentSize().height/2));
+
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Vec2::ZERO);
+    this->addChild(menu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    
+    // position the label on the center of the screen
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
+
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+
+    // add "HelloWorld" splash screen"
+    auto sprite = Sprite::create("bg.png");
+
+    // position the sprite on the center of the screen
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+    
+    SpriteFrameCache * frameCache = SpriteFrameCache::getInstance();
+    frameCache->addSpriteFramesWithFile("sprite_sheet.plist", "sprite_sheet.png");
+    auto player = Sprite::createWithSpriteFrameName("player_1.png");
+    player->setPosition(visibleSize.width/2, visibleSize.height/2);
+    this->addChild(player);
+    
+    Animation * animation = Animation::create();
+    SpriteFrame * frame;
+    int i;
+    for (i=1; i<=3; i++) {
+        char szName[100] = {0};
+        sprintf(szName, "player_%i.png",i);
+        frame = SpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName(szName);
+        animation->addSpriteFrame(frame);
+    }
+    //animation->setDelayPerUnit(0.2f/0.3f);
+    animation->setDelayPerUnit(2.8f/14.0f);
+    animation->setRestoreOriginalFrame(true);
+    animation->setLoops(-1);//使动画不断的执行
+    
+    auto action = Animate::create(animation);
+    player->runAction(Sequence::create(action,action->reverse(),NULL));
+    
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
